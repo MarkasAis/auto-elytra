6c52efc57e84b09002d96585eb728d3028499533
MarkAis <mark.aisparas@gmail.com>
2021-11-07 14:29:33 +0100

    Setup environment

--- src/main/java/net/markais/autoelytra/mixin/AutoElytraMixin.java
+++ src/main/java/net/markais/autoelytra/mixin/AutoElytraMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.markais.autoelytra.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.markais.autoelytra.AutoElytra;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class AutoElytraMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		AutoElytra.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
